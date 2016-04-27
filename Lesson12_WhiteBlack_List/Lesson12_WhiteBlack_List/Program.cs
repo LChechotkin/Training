73a763a936c9fe36975631555c8ca7df7a4440db
LChechotkin <Lchechotkin@gmail.com>
2016-04-27 16:16:39 +0300

    Lesson 12 add/commit update 2

--- Lesson12_WhiteBlack_List/Lesson12_WhiteBlack_List/Program.cs
+++ Lesson12_WhiteBlack_List/Lesson12_WhiteBlack_List/Program.cs
@@ -18,12 +18,28 @@ namespace Lesson12_WhiteBlack_List
             FilterOut.BlackFilter(@"..\..\input.txt", @"..\..\black_output.txt", @"..\..\blacklist.txt");
             FilterOut.BlackFilter(@"..\..\white_output.txt", @"..\..\black_white_output.txt", @"..\..\blacklist.txt");
 
-            
+            //Filtering with white list
             FilterOut.PrintFile(@"..\..\input.txt");
             FilterOut.PrintFile(@"..\..\white_output.txt");
             FilterOut.PrintFile(@"..\..\whitelist.txt");
+
+            
+            Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
 
+            //Filtering with black list
+            FilterOut.PrintFile(@"..\..\black_output.txt");
+            FilterOut.PrintFile(@"..\..\blacklist.txt");
+            
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+            //Filtering with black and white list
+            FilterOut.PrintFile(@"..\..\black_white_output.txt");
+
+            Console.ReadKey();
+            
 
         }
     }
